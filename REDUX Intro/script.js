24d61bd2ca2196e58c5013dd58902ceec6fc1258
Allan Oguis <allanoguis@gmail.com>
2024-11-25 06:37:46 +0800

    modified:   REDUX Intro/script.js 	new file:   REDUX exercise - Use spread operators/script.js 	new file:   REDUX exercise - copy object with object.assign/script.js 	new file:   REDUX exercise - increment decrement/script.js 	new file:   REDUX exercise - never mutate state/script.js 	new file:   REDUX exercise - remove item from array/script.js 	new file:   script.js

--- REDUX Intro/script.js
+++ REDUX Intro/script.js
@@ -9,3 +9,8 @@ interface UserState {
 // Actions
 const increment = { type: "INCREMENT" };
 const incrementByAmount = { type: "INCREMENT", payload: 10 };
+
+// Reducers
+// Responsible for the type of the action and use payload to make updates to the Store
+
+// *important* always make a copy of the state before making changes to the state. kinda like classes or constructors
