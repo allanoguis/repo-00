2d207fc7db2bc1541dbd228c1feaa83e2ea4a482
Allan Oguis <allanoguis@gmail.com>
2024-11-28 01:50:07 +0800

    modified:   script.js

--- script.js
+++ script.js
@@ -1,91 +1,9 @@
-// Redux:
-const ADD = "ADD";
+const dataset = [12, 31, 22, 17, 25, 18, 29, 14, 9];
 
-const addMessage = (message) => {
-  return {
-    type: ADD,
-    message: message,
-  };
-};
-
-const messageReducer = (state = [], action) => {
-  switch (action.type) {
-    case ADD:
-      return [...state, action.message];
-    default:
-      return state;
-  }
-};
-
-const store = Redux.createStore(messageReducer);
-
-// React:
-const Provider = ReactRedux.Provider;
-const connect = ReactRedux.connect;
-
-// Change code below this line
-class Presentational extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      input: "",
-      messages: [],
-    };
-    this.handleChange = this.handleChange.bind(this);
-    this.submitMessage = this.submitMessage.bind(this);
-  }
-  handleChange(event) {
-    this.setState({
-      input: event.target.value,
-    });
-  }
-  submitMessage() {
-    this.setState((state) => ({
-      input: "",
-      messages: state.messages.concat(state.input),
-    }));
-  }
-  render() {
-    return (
-      <div>
-        <h2>Type in a new Message:</h2>
-        <input value={this.state.input} onChange={this.handleChange} />
-        <br />
-        <button onClick={this.submitMessage}>Submit</button>
-        <ul>
-          {this.state.messages.map((message, idx) => {
-            return <li key={idx}>{message}</li>;
-          })}
-        </ul>
-      </div>
-    );
-  }
-}
-// Change code above this line
-
-const mapStateToProps = (state) => {
-  return { messages: state };
-};
-
-const mapDispatchToProps = (dispatch) => {
-  return {
-    submitNewMessage: (message) => {
-      dispatch(addMessage(message));
-    },
-  };
-};
-
-const Container = connect(mapStateToProps, mapDispatchToProps)(Presentational);
-
-class AppWrapper extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    return (
-      <Provider store={store}>
-        <Container />
-      </Provider>
-    );
-  }
-}
+d3.select("body")
+  .selectAll("div")
+  .data(dataset)
+  .enter()
+  .append("div")
+  .attr("class", "bar")
+  .style("height", (d) => d + "px");
